777578d2c9c15253539be3be358104bb8144bd2d
Flávio Pinheiro <fpssouzza@gmail.com>
2019-08-08 18:04:27 -0300

    Criação do Caso1 - Funcionando

---
+++ Caso1/Processo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace Caso1
+{
+    class Processo
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll")]
+        private static extern Int32 GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+        public static Process GetActiveProcess()
+        {
+            IntPtr hwnd = GetForegroundWindow();
+            return hwnd != null ? GetProcessByHandle(hwnd) : null;
+        }
+        private static Process GetProcessByHandle(IntPtr hwnd)
+        {
+            try
+            {
+                uint processID;
+                GetWindowThreadProcessId(hwnd, out processID);
+                return Process.GetProcessById((int)processID);
+            }
+            catch { return null; }
+        }
+    }
+}
---
+++ Caso1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Caso1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Process currentProcess = Processo.GetActiveProcess();
+                if (currentProcess != null)
+                {
+                    Console.WriteLine(currentProcess.MainWindowTitle);
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
